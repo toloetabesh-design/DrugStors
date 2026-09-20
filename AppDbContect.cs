63983feee745d548edac6787013975ee54728a54
toloetabesh-design <toloetabesh@gmail.com>
2026-09-20 11:18:52 +0330

    Add files via upload

---
+++ AppDbContect.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DragStore.Domain.Entities;
+
+namespace DragStore.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Drug> Drugs { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        internal void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class DbContextOptions<T>
+    {
+    }
+
+    public class DbContext
+    {
+        public DbContext(DbContextOptions<AppDbContext> options)
+        {
+            Options = options;
+        }
+
+        public DbContextOptions<AppDbContext> Options { get; }
+    }
+}
